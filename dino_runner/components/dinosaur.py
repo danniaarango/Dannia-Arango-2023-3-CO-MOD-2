687837dfba47e44ec6455f4cf1442c49c67c5d32
danniaarango <132022514+danniaarango@users.noreply.github.com>
2023-04-30 16:30:25 -0500

    Class 2: Homework 1

--- dino_runner/components/dinosaur.py
+++ dino_runner/components/dinosaur.py
@@ -1,22 +1,28 @@
 import pygame
 
 from pygame.sprite import Sprite
-from dino_runner.utils.constants import RUNNING, JUMPING
+from dino_runner.utils.constants import RUNNING, JUMPING, DUCKING
 
 class Dinosaur:
     X_POS = 80
     Y_POS = 310
+    Y_DUCK = 360
     JUMP_SPEED = 8.5
+    DUCK_SPEED = 8.5
 
     def __init__(self):
         self.image = RUNNING[0]
+        self.image = DUCKING[0]
         self.dino_rect = self.image.get_rect()
         self.dino_rect.x = self.X_POS
         self.dino_rect.y = self.Y_POS
+        self.dino_rect.y = self.Y_DUCK
         self.dino_run = True
         self.step_index = 0
         self.dino_jump = False
         self.jump_speed = self.JUMP_SPEED 
+        self.dino_duck = False
+        self.duck_speed = self.DUCK_SPEED
         
 
     def update(self, user_imput):
@@ -30,13 +36,23 @@ class Dinosaur:
 
         #si el dino esta saltando es True
         if self.dino_jump :
-            self.jump()
+            self.jump()   
 
         if user_imput[pygame.K_UP] and not self.dino_jump:
             self.dino_jump = True
             self.dino_run = False
         elif not self.dino_jump:
             self.dino_jump = False
+            self.dino_run = True
+
+        if self.dino_duck :
+            self.duck()
+        
+        if user_imput[pygame.K_DOWN] and not self.dino_duck:
+            self.dino_duck = True
+            self.dino_run = False
+        elif self.dino_duck:
+            self.dino_duck = False
             self.dino_run = True
         
     def draw(self, screen):
@@ -58,3 +74,19 @@ class Dinosaur:
             self.dino_rect.y = self.Y_POS
             self.dino_jump = False
             self.jump_speed = self.JUMP_SPEED
+
+    def duck(self):
+        self.image = DUCKING[0] if self.step_index < 5 else DUCKING[1]
+        self.dino_rect = self.image.get_rect()
+        self.dino_rect.x = self.X_POS
+        self.dino_rect.y = self.Y_DUCK
+        self.dino_duck = False
+        self.step_index += 1 
+        
+        
+        
+
+
+        
+
+
